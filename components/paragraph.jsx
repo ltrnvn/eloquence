4018b24e8daf4e1d370dfbeb069e9db3e8e179b1
Léon Tran-Van <ephalys@live.fr>
2022-08-06 00:22:36 +0200

    fix: wording home & games pages + animations on paragraphs

--- components/paragraph.jsx
+++ components/paragraph.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Paragraph = (props) => {
     return (
-        <p className="animate-enter opacity-0 animation-delay-150">
+        <p
+            className="mb-6 animate-enter opacity-0"
+            style={{
+                // Setting a maximum delay of 750ms
+                animationDelay: `${
+                    (props.delay < 5 ? props.delay + 1 : 6) * 150
+                }ms`,
+            }}
+        >
             {props.children}
         </p>
     )
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,15 +9,17 @@ import H1Title from '../components/h1Title'
 import H2Title from '../components/h2Title'
 import Paragraph from '../components/paragraph'
 
-const components = {
-    h1: H1Title,
-    h2: H2Title,
-    p: Paragraph,
-    a: ExternalLink,
-    em: (props) => <em {...props} className="font-serif italic text-xl" />,
-}
-
 const _app = ({ Component, pageProps }) => {
+    let paragraphCount = 0
+    const components = {
+        h1: H1Title,
+        h2: H2Title,
+        h3: (props) => <h3 {...props} className="mt-12 mb-6 text-gray-600" />,
+        p: (props) => <Paragraph {...props} delay={paragraphCount++} />,
+        a: ExternalLink,
+        em: (props) => <em {...props} className="font-serif italic text-xl" />,
+    }
+
     return (
         <MDXProvider components={components}>
             <Head>
