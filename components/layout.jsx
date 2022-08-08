05229d0cb2d4f3eeca78fe371340d23ad580652c
Léon Tran-Van <ephalys@live.fr>
2022-08-08 23:53:55 +0200

    fix: layout style on mobile

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,7 +6,7 @@ import Toast from './toast'
 const Layout = (props) => {
     return (
         <>
-            <main className="max-w-3xl mx-auto px-4 py-28 sm:py-40">
+            <main className="max-w-3xl mx-auto px-4 py-12 sm:py-40">
                 <Menu>
                     {props.title && (
                         <h1 className="font-bold text-h1 text-black dark:text-white">
