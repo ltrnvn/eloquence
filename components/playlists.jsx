c2315cb936ca33c8ac8dabab3026003393d69216
Léon Tran-Van <ephalys@live.fr>
2022-08-22 19:04:18 +0200

    fix: fetch playlists on build

--- components/playlists.jsx
+++ components/playlists.jsx
@@ -1,47 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { getPlaylists } from '/utils/spotify'
+import React from 'react'
 import Image from 'next/image'
 
-const Playlists = () => {
-    const [playlists, setPlaylists] = useState([])
-
-    useEffect(() => {
-        getPlaylists().then((playlists) => {
-            setPlaylists(playlists)
-        })
-    }, [])
-
-    const RenderPlaylists = () => {
-        return playlists.map((playlist, i) => (
-            <li
-                key={i}
-                className="border-b-[1px] border-gray-700 last:border-0 opacity-0 animate-enter "
-                style={{ animationDelay: i * 100 + 'ms' }}
+const RenderPlaylists = ({ playlists }) => {
+    return playlists.map((playlist, i) => (
+        <li
+            key={i}
+            className="border-b-[1px] border-gray-700 last:border-0 opacity-0 animate-enter "
+            style={{ animationDelay: i * 100 + 200 + 'ms' }}
+        >
+            <a
+                className="flex items-center py-2 transition-opacity group-hover:opacity-30 group-hover:hover:opacity-100"
+                href={playlist.url}
+                target="_blank"
             >
-                <a
-                    className="flex items-center py-2 transition-opacity group-hover:opacity-30 group-hover:hover:opacity-100"
-                    href={playlist.url}
-                    target="_blank"
-                >
-                    <Image
-                        src={playlist.image}
-                        alt="playlist image"
-                        width={50}
-                        height={50}
-                    />
-                    <div className="flex flex-col ml-4">
-                        <p className="leading-6">{playlist.name}</p>
-                        <p className="text-gray-500 text-sm">
-                            {playlist.description}
-                        </p>
-                    </div>
-                </a>
-            </li>
-        ))
-    }
+                <Image
+                    src={playlist.image}
+                    alt="playlist image"
+                    width={50}
+                    height={50}
+                />
+                <div className="flex flex-col ml-4">
+                    <p className="leading-6">{playlist.name}</p>
+                    <p className="text-gray-500 text-sm">
+                        {playlist.description}
+                    </p>
+                </div>
+            </a>
+        </li>
+    ))
+}
+
+const Playlists = ({ playlists }) => {
     return (
         <ul className="mt-12 group">
-            <RenderPlaylists />
+            <RenderPlaylists playlists={playlists} />
         </ul>
     )
 }
