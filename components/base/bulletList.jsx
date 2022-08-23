c137fb8ea7a857e740f10ad954325903d6cc77da
Léon Tran-Van <ephalys@live.fr>
2022-08-24 01:18:07 +0200

    fix: bullet list margins

--- components/base/bulletList.jsx
+++ components/base/bulletList.jsx
@@ -11,7 +11,7 @@ const BulletList = (props) => {
                             key={i}
                             className="list-disc pl-6 marker:text-gray-300 dark:marker:text-gray-600"
                         >
-                            <li>{item}</li>
+                            <li className="pl-2">{item}</li>
                         </ul>
                     )
                 })}
