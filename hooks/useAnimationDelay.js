7371456eab393e54ab804111ba537dbcef98c3ff
Léon Tran-Van <ephalys@live.fr>
2022-08-24 17:13:47 +0200

    refactor: useMemo instead of useEffect in animationDelay

--- hooks/useAnimationDelay.js
+++ hooks/useAnimationDelay.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useMemo } from 'react'
 
 export const BASE_DELAY = 100
 export const MAX_ITEMS = 5
@@ -7,7 +7,7 @@ export const MAX_DELAY = MAX_ITEMS * BASE_DELAY
 export default function useAnimationDelay(itemCount) {
     const [delay, setDelay] = useState(null)
 
-    useEffect(() => {
+    useMemo(() => {
         //If 5+ items are rendered, delay will not exceed 600ms
         setDelay(
             (itemCount < MAX_ITEMS ? itemCount + 1 : MAX_ITEMS + 1) * BASE_DELAY
