ee04846b53ce032b03ace0d02c178ba24d0892f6
Léon Tran-Van <ephalys@live.fr>
2022-08-29 12:33:54 +0200

    refactor: remove useless components and use richList directly in .mdx files

--- components/gameslist.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RichList from '@/components/richList';
-import { games } from '@/utils/games';
-
-const Gameslist = () => {
-    return <RichList items={games} />;
-};
-
-export default Gameslist;
--- components/playlists.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RichList from '@/components/richList';
-
-const Playlists = ({ playlists }) => {
-    return <RichList items={playlists} />;
-};
-
-export default Playlists;
