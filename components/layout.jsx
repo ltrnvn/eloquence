cc226a8a929227a0672e3acf357d516cfaa9ee8b
Léon Tran-Van <ephalys@live.fr>
2023-03-20 20:07:04 +0100

    refactor: move layout component constants to prevent the component from recreating them at each render

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,11 +3,33 @@ import Footer from './footer';
 import MenuContainer from '@/components/menu/menuContainer';
 import { NextSeo } from 'next-seo';
 
+const DEFAULT_TITLE =
+    'Léon Tran-Van — Frontend engineer with a designer soul curbed by color blindness';
+const DESCRIPTION =
+    'Building curated projects and beautiful interfaces with the benefits of cutting-edge technology.';
+
+const OPENGRAPH_CONFIG = {
+    url: 'https://www.tranvanleon.com/',
+    title: DEFAULT_TITLE,
+    description: DESCRIPTION,
+    images: [
+        {
+            url: 'https://www.tranvanleon.com/cover.png',
+            width: 1600,
+            height: 1010,
+        },
+    ],
+    site_name: 'Léon Tran-Van',
+    type: 'profile',
+    profile: {
+        firstName: 'Léon',
+        lastName: 'Tran-Van',
+        username: 'ephalys',
+        gender: 'male',
+    },
+};
+
 const Layout = ({ children, SEOTitle }) => {
-    const DEFAULT_TITLE =
-        'Léon Tran-Van — Frontend engineer with a designer soul curbed by color blindness';
-    const DESCRIPTION =
-        'Building curated projects and beautiful interfaces with the benefits of cutting-edge technology.';
     return (
         <>
             <NextSeo
@@ -15,26 +37,7 @@ const Layout = ({ children, SEOTitle }) => {
                 titleTemplate="%s — Léon Tran-Van"
                 defaultTitle={DEFAULT_TITLE}
                 description={DESCRIPTION}
-                openGraph={{
-                    url: 'https://www.tranvanleon.com/',
-                    title: DEFAULT_TITLE,
-                    description: DESCRIPTION,
-                    images: [
-                        {
-                            url: 'https://www.tranvanleon.com/cover.png',
-                            width: 1600,
-                            height: 1010,
-                        },
-                    ],
-                    site_name: 'Léon Tran-Van',
-                    type: 'profile',
-                    profile: {
-                        firstName: 'Léon',
-                        lastName: 'Tran-Van',
-                        username: 'ephalys',
-                        gender: 'male',
-                    },
-                }}
+                openGraph={OPENGRAPH_CONFIG}
             />
             <main className="max-w-2xl mx-auto px-4 py-12 sm:pt-40 sm:pb-28">
                 <MenuContainer />
