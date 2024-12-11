3bd5474389563bc7d47319d7c7bec88b82cdf485
Léon Tran-Van <contact@leontranvan.com>
2024-12-11 07:13:08 +0100

    fix(socials): new `GitHub` link

--- utils/social.js
+++ utils/social.js
@@ -2,12 +2,12 @@ export const social = [
     {
         id: 'linkedin',
         name: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/leon-tran-van/',
+        url: 'https://www.linkedin.com/in/leon-tran-van',
     },
     {
         id: 'github',
         name: 'GitHub',
-        url: 'https://github.com/Ephalys/',
+        url: 'https://github.com/ltrnvn',
     },
     {
         id: 'spotify',
